3dcb220e0fbc52d612f60f20d1052c112c7833e4
chunyukuo88 <alexgochenour@gmail.com>
2020-01-16 19:58:44 -0500

    The algorithm can correctly process the top row!

--- src/js/algorithm.js
+++ src/js/algorithm.js
@@ -4,17 +4,25 @@ module.exports = {
 
     evaluateDish(generalArray){
         
-        let myMap = new Map()
-
+        let neighborsArray = [];
 
         for (let index = 0; index < 8; index++) {
             let neighbors = 0;
-            if (generalArray[index-1] === 1){neighbors++;}
-            if (generalArray[index+1] === 1){neighbors++;}
-                         
-            console.log(neighbors);
+            if (generalArray[index-1] == 1){neighbors++;}
+            if (generalArray[index+1] == 1){neighbors++;}
+            neighborsArray.push(neighbors);
         }
 
+        console.log(neighborsArray);
+
+        for (let index = 0; index < generalArray.length; index++) {
+            
+            if(generalArray[index] == 1 && neighborsArray[index] == 2){
+                generalArray[index] = 1;
+            }  else {
+                generalArray[index] = 0;
+            }
+        }
 
 
         return generalArray;
