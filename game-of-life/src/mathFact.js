b02b2ae6e07f901d52b0dcd3000c01031b334884
chunyukuo88 <alexgochenour@gmail.com>
2020-04-24 11:13:37 -0400

    Fixed the fetch() call in mathFact.js. It was using the .json() method instead of the .text() method, since the response from the host is a string.

--- game-of-life/src/mathFact.js
+++ game-of-life/src/mathFact.js
@@ -1,10 +1,9 @@
 export async function getFact(integer){
     let url = `http://numbersapi.com/${integer}/math`;
-    const options = { method: "GET", headers: { "Content-type": "application/JSON" }};
-    let mathFact; 
-    fetch(url, options)
-        .then(res => res.json())
-        .then(data => {
-            mathFact = data
-        });
+    const options = { method: "GET", headers: { "Content-type": "text/plain" }};
+    
+    let mathFact = await fetch(url, options)
+        .then(res => res.text());
+    console.log(mathFact);
+    return mathFact;
 };
--- game-of-life/src/tests/mathFact.test.js
+++ game-of-life/src/tests/mathFact.test.js
@@ -3,9 +3,9 @@ import { getFact } from '../mathFact';
 describe('math.js', ()=>{
     describe('When given an integer, ', ()=> {
         test('It gets a fact from the Numbers API', async () => {
-            let result = await getFact(5);
-            const expectedResult = '5 is the number of Platonic solids.';
-            expect(result).toEqual(expectedResult);
+            let mathFact = await getFact(5);
+            let result = mathFact.includes('5 is'); //Owing to the fact that there are multiple facts for this integer.
+            expect(result).toBe(true);
         });
     });
 });
