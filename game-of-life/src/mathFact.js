ad3f6e3a5f8f559325cb2b8a21ed25d65ffcbfb9
chunyukuo88 <alexgochenour@gmail.com>
2020-05-16 23:31:29 -0400

    Minor but important refactoring.

--- game-of-life/src/mathFact.js
+++ game-of-life/src/mathFact.js
@@ -1,7 +1,9 @@
 
 
-export default async function mathFact(integer){
+const mathFact = async integer => {
     const url = `http://numbersapi.com/${integer}/math`;
     const result = await fetch(url).then(res => res.text());
     return result;
 };
+
+export default mathFact;
