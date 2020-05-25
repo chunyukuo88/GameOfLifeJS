73d8ef9f846fded5355522610326a319f5be28f4
chunyukuo88 <alexgochenour@gmail.com>
2020-05-25 18:19:58 -0400

    Figured out the ratios and how to fill the screen: Using vw and vh.

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -11,21 +11,36 @@ const App = () => {
 
   return (
     <div className='App-wrapper' ref={ref}>
-      <h1 onClick={() => console.log(ref.current.className)}>Chunyu Kuo's Splendid Maths</h1>
-      <button className='count-up'
-              onClick={() => setCount(count + 1)}
-      >
-        Count up!
-      </button>
-      <input type="number" onChange={(e) => setCount(e.target.value)} value={count}/>
-      <button className='count-down' 
-              onClick={() => setCount(count - 1)}
-      >
-        Count down!
-      </button>
-      <p><span>Now here is a fun fact about the number </span><span>{count}:</span></p>
 
-      <MathFact count={count} />
+        <section className='upper-left'>
+          <h1 className='page-title' onClick={() => console.log(ref.current.className)}>Chunyu Kuo's Splendid Maths</h1>
+        </section>
+
+        <section className='upper-right'>
+          <div className='burger'>
+            Replace with hamburger element
+          </div>
+          <section className='switch-section'>
+            <p className='localization'>Localization</p>
+          </section>
+        </section>
+
+
+        <section className='lower-half'>
+          <button className='count-up'
+                  onClick={() => setCount(count + 1)}
+          >
+            Count up!
+          </button>
+          <input type="number" onChange={(e) => setCount(e.target.value)} value={count}/>
+          <button className='count-down' 
+                  onClick={() => setCount(count - 1)}
+          >
+            Count down!
+          </button>
+          <p><span>Now here is a fun fact about the number </span><span>{count}:</span></p>
+          <MathFact count={count} />
+        </section>
     </div>
   );
 }
