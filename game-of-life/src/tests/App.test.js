1405c87be61da8cde0550da6ccfff3b3f70fa949
chunyukuo88 <alexgochenour@gmail.com>
2020-06-18 23:05:13 -0400

    Shallow rendering

--- game-of-life/src/tests/App.test.js
+++ game-of-life/src/tests/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('Renders the Learn React link.', ()=>{
-
+it('renders the App component and console logs it.', ()=>{
+  const wrapper = shallow(<App />);
+  console.log(`===\n${wrapper.debug()}\n===`);
 });
