c1b74cb7e8ed80e69641716e34a0b27d080263e7
chunyukuo88 <alexgochenour@gmail.com>
2020-06-18 22:03:14 -0400

    Configured Enzyme, empty test passes.

--- game-of-life/src/tests/App.test.js
+++ game-of-life/src/tests/App.test.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-describe('App.js -- Rendering strings, arrays and imported modules.', ()=>{
-  test('Renders heading', () => {
-    const { getByText } = render(<App />);
-    const titleElement = getByText('The Splendid Maths');
-    expect(titleElement).toBeInTheDocument();
-  });
-  test('Renders fun fact header', () => {
-    const { getByText } = render(<App />);
-    const funFactHeader = getByText('Now here is a fun fact about the number');
-    expect(funFactHeader).toBeInTheDocument();
-  });
+test('Renders the Learn React link.', ()=>{
+
 });
